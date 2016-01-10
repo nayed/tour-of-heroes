fc033e3b4e7221906cec6fe189fefe7e348a4665
Nayed Saïd Ali <nayed.saidali@gmail.com>
2016-01-10 18:12:30 +0100

    hi angular 2

---
+++ app/app.component.ts
@@ -0,0 +1,8 @@
+import {Component} from 'angular2/core'
+
+@Component({
+    selector: 'my-app',
+    template: '<h1>My First Angular 2 App</h1><p>Let\'s see where all the hype comes</p>'
+})
+
+export class AppComponent {}
