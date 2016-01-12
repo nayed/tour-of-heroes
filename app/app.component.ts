2603fca12527498d7376f3eae59ab7e363bf7437
Nayed Saïd Ali <nayed.saidali@gmail.com>
2016-01-12 13:53:15 +0100

    some refac

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,13 +1,11 @@
 import {Component} from 'angular2/core'
-
-interface Hero {
-    id: number
-    name: string
-}
+import {Hero} from './hero'
+import {HeroDetailComponent} from './hero-detail.component'
 
 @Component({
     selector: 'my-app',
     template: `
+        <h1>{{title}}</h1>
         <h2>My Heroes</h2>
         <ul class="heroes">
             <li *ngFor="#hero of heroes" 
@@ -16,15 +14,7 @@ interface Hero {
                 <span class="badge">{{hero.id}}</span> {{hero.name}}
             </li>
         </ul>
-
-        <div *ngIf="selectedHero">
-            <h2>{{selectedHero.name}} details!</h2>
-            <div><label>id:  </label>{{selectedHero.id}}</div>
-            <div>
-                <label>name: </label>
-                <input [(ngModel)]="selectedHero.name" placeholder="name"/>
-            </div>
-        </div>
+        <my-hero-detail [hero]="selectedHero"></my-hero-detail>        
     `,
     styles:[`
     .selected {
@@ -73,7 +63,8 @@ interface Hero {
       margin-right: .8em;
       border-radius: 4px 0px 0px 4px;
     }
-  `]
+  `],
+    directives: [HeroDetailComponent]
 })
 
 export class AppComponent {
